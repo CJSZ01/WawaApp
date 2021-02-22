c7bc397a1d6b1da7b0e3ead4fc125a1d30b87b9f
CJSZ01 <56030569+CJSZ01@users.noreply.github.com>
2021-02-21 22:20:29 -0300

    Implemented Available Networks

--- src/app/pages/network-test/network-test.page.ts
+++ src/app/pages/network-test/network-test.page.ts
@@ -11,6 +11,9 @@ export class NetworkTestPage implements OnInit {
   public currentNetwork: string;
   public savedNetworks;
   public showSaved: boolean = false;
+  public availableNetworks;
+  public showAvailable: boolean = false;
+  public scanning: boolean = false;
 
   constructor(private wifiWizard2: WifiWizard2) {
     this.wifiWizard2.getConnectedSSID().then(ssid => { this.currentNetwork = ssid; });
@@ -30,7 +33,25 @@ export class NetworkTestPage implements OnInit {
     }else{
       this.showSaved = false;
     }
-    console.log(this.showSaved)
+  }
+
+  public toggleAvailable(){
+    if (!this.showAvailable ){
+      this.showAvailable = true;
+    }else{
+      this.showAvailable = false;
+    }
+  }
+
+  public async scanNetworks(){
+    this.toggleAvailable();
+    if(this.showAvailable){
+      this.scanning = true;
+      await this.wifiWizard2.scan().then(scanResults => {this.availableNetworks = scanResults});
+      this.scanning = false;
+      console.log(this.availableNetworks);
+    }
+
   }
 
 }
